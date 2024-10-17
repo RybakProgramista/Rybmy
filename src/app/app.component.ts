e6ffef8a7bef354833f85687d49719c1de4c8fbf
kapema <kacperlewandowski491@gmail.com>
2024-10-17 15:44:51 +0200

    pasek wow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainSceneComponent } from './main-scene/main-scene.component';
+import { LineComponent } from './line/line.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainSceneComponent],
+  imports: [RouterOutlet, MainSceneComponent, LineComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent{
-  
-}
+export class AppComponent {}
