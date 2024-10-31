925bce55d66da70d5f3692fa47e4cf3d420ec7de
JakubDrzala <96517502+JakubDrzala@users.noreply.github.com>
2024-10-31 13:01:12 +0100

    początki łączenia bazy z serwerem

--- backend/server/app.js
+++ backend/server/app.js
@@ -5,17 +5,6 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const fishes = ["karp", "lin", "leszcz"];
-
-app.get('/fishes', (req, res) => {
-    res.send(fishes)
-  })
-
-  app.get('/fishes/:id', (req, res) => {
-    let id = parseInt(req.params.id);
-    res.send({rybka: fishes[id]})
-  })
-
 const port = 2115;
 app.listen(port, () =>{
     console.log("Nasłuchuje na porcie " + port);
