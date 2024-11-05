d0a1fddc492c97baaa067cfae0c9c2c62fc02f8e
goltOV <146551244+goltOV@users.noreply.github.com>
2024-11-05 08:39:11 +0100

    connection based on new main branch

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,22 +1,46 @@
 const express = require("express")
+const mysql = require('mysql');
 const app = express();
 const cors = require("cors");
+const port = 3000
 
 app.use(cors());
 app.use(express.json());
 
-const fishes = ["karp", "lin", "leszcz"];
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'rybcie'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL: ' + err.stack);
+    return;
+  }
+  console.log('Connected to MySQL as ID ' + db.threadId);
+});
 
 app.get('/fishes', (req, res) => {
-    res.send(fishes)
+  db.query('SELECT * FROM `ryby`', function (error, results) {
+    if (error) throw error;
+    res.send(results);
   })
+})
 
-  app.get('/fishes/:id', (req, res) => {
-    let id = parseInt(req.params.id);
-    res.send({rybka: fishes[id]})
-  })
+// const fishes = ["karp", "lin", "leszcz"];
 
-const port = 2115;
+// app.get('/fishes', (req, res) => {
+//     res.send(fishes)
+//   })
+
+// app.get('/fishes/:id', (req, res) => {
+//   let id = parseInt(req.params.id);
+//   res.send({rybka: fishes[id]})
+// })
+
+// const port = 2115;
 app.listen(port, () =>{
     console.log("Nasłuchuje na porcie " + port);
 });
