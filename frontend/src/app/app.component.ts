65ff16c7de27cae6d514fb47fcd47219e54af216
kapema <kapeciuinstagram@gmail.com>
2024-11-21 14:10:43 +0100

    odzyskanie brancha
    dolbajob

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -20,95 +20,7 @@ export class AppComponent{
   //SERWER
   constructor(private dataService: DataService, private http: HttpClient){}
   ngOnInit(){
-    const server = 'http://localhost:3000/'
-    let id: number | null = null
-
-
-    //pobieranie z JSON
-    id = 2
-    this.http.get<Equip>(server+'api/get/kolowrotek?id='+id)
-    .subscribe(e => {
-      console.log(e)
-      console.log('nazwa:'+e.nazwa);
-      
-    })
-    this.http.get<Equip>(server+'api/get/wedka?id='+id)
-    .subscribe(e => {
-      console.log(e)
-      console.log('nazwa:'+e.nazwa);
-    })
-    this.http.get<Equip>(server+'api/get/zylka?id='+id)
-    .subscribe(e => {
-      console.log(e)
-      console.log('nazwa:'+e.nazwa);
-    })
-
-
-    //pobieranie z tablicą 
-    this.http.get<Equip>(server+'api/getTable/kolowrotek?id='+id)
-    .subscribe(e => {
-      console.log(e)
-      
-    })
-    this.http.get<Equip>(server+'api/getTable/wedka?id='+id)
-    .subscribe(e => {
-      console.log(e)
-    })
-    this.http.get<Equip>(server+'api/getTable/zylka?id='+id)
-    .subscribe(e => {
-      console.log(e)
-    })
-
     
-    //pobiera tablicy z rybami i wypisuje ich nazwy
-    // this.http.get<Array<Fish>/*wpisjes jakiego typu będzie pobierana wartość z servera */>(server+'fishes'/*ścieżka do servera*/).subscribe(e => {
-    //   e.forEach(row => {  
-    //     console.log(row.nazwa); //jest wysylana jako json więc możesz z każdego wiersz wybrać wartość przypisaną do 'nazwa'
-        
-    //   })
-    // })
-
-
-
-
-
-    //logowanie
-    // let login = 'kacper', password = 'aaaa'
-    // this.http.get<{id: number}|null>(server+'api/get/login?login='+login+'&password='+password)
-    //   .subscribe(e => {
-    //     if (e!==null) {
-    //       id = e.id
-    //       console.log(e.id);
-    //       console.log("logowanie udalo sie");
-
-    //       //pobiera i wyświetla znajomych
-    //       this.http.get<Array<Player>|null>(server+'znajomi?id='+id)
-    //         .subscribe(e => {
-    //           if(e===null)  console.log("nie masz znajomych");
-    //           else{
-    //             e.forEach(friend => {
-    //               console.log(friend);
-    //             })
-    //           }
-    //         })
-
-    //     }else console.log("logowanie nie udalo sie");
-    // })
-    
-    //   fetch(server+'login?login='+login+'&password='+password)
-    //   .then(result => console.log(result))
-    
-
-    
-  
-    
-    // fetch(server+'znajomi?id='+id)
-    //   .then(response => response.json())
-    //   .then(friends => friends.array.forEach((name: any) => {
-    //     console.log(name);
-        
-    //   }))
-      
   }
 
   returnedItem = ["NAZWA", "WYTRZYMAŁOŚĆ", "CZY KUPIONY"]; //<- TU MA BYĆ KURWA ZWRACANY OLEK
@@ -138,32 +50,4 @@ export class AppComponent{
   changeCurrDurability(newVal : number){
     this.currDurabilityPercent = newVal;
   }
-}
-
-
-interface Player {
-  id: number
-  nazwa: string
-  doswiadczenie: number
-}
-
-interface Fish {
-  id: number;
-  nazwa: string
-  obrazek: string
-  minKg: number
-  maxKg:  number
-  minWymiar: number
-  maxWymiar: number
-  cena: number
-  doswiadczenie: number
-  wystepowanie: string
-  opis: string
-}
-
-interface Equip {
-  id: number
-  nazwa: string
-  wytrzymalosc: number
-  cena: number
 }
