3c1a8247255ad13ae9fd63eeb3cf01ba065e38e5
kapema <145340839+kapema@users.noreply.github.com>
2024-11-28 14:51:47 +0100

    pos to buy

--- backend/server/controllers/get/possibilityToBuy.js
+++ backend/server/controllers/get/possibilityToBuy.js
@@ -4,10 +4,27 @@ import database from '../../database.js'
 export const possibilityToBuy = (req, res) =>{
     const {id, type, playerId} = req.query
     let cena
-    database.query('SELECT `cena` FROM `'+type+'` WHERE `id` = ?;',[id], function (error, results) {
-        if (error) res.json(null)
+    database.query('SELECT `cena` FROM `'+type+'` WHERE `id` = ?;',[id], function (error1, cenaRes) {
+        if (error1) res.json(null)
         else{
-            cena = results[0]
+            cena = parseInt(cenaRes[0]['cena'])
+            console.log(1);
+            
+            database.query('SELECT `pieniadze` FROM `gracz` WHERE `idGracz`=?;',[playerId], function (error2, pieniadzeGracza){
+                if (error2) res.json(error2)
+                    else{
+                console.log(2);
+                
+                        let pieniadze = parseInt(pieniadzeGracza[0]['pieniadze'])
+                        console.log(cena);
+                        console.log(pieniadze);
+                        
+                        
+                        if(cena <= pieniadze)
+                            res.json(true)
+                        else res.json(false)
+                    }
+            })
         }
     })
 }
--- backend/server/routes/equip.js
+++ backend/server/routes/equip.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import {equip} from "../controllers/get/equip.js"
 import {equipT} from "../controllers/getTable/equipT.js"
+import {possibilityToBuy} from "../controllers/get/possibilityToBuy.js"
 const router = Router()
 
 router.get('/get/equip', equip)
 
 router.get('/getTable/equip', equipT)
+
+router.get('/get/possibilityToBuy', possibilityToBuy)
 
 export default router
