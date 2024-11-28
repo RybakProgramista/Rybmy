2dc60424fc934e15961cc19ae022e18eeddaa390
kapema <145340839+kapema@users.noreply.github.com>
2024-11-28 15:38:35 +0100

    fjsdoiosidhg

--- backend/server/controllers/get/equip.js
+++ backend/server/controllers/get/equip.js
@@ -2,11 +2,30 @@ import database from '../../database.js'
 
 
 export const equip = (req, res) =>{
-    const {id, type} = req.query
-    database.query('SELECT * FROM `'+type+'` WHERE `id` = ?;',[id], function (error, results) {
-        if (error) res.json(null)
+    const {id, type, playerId} = req.query
+    database.query('SELECT * FROM `'+type+'` WHERE `id` = ?;',[id], function (error, results1) {
+        if (error) res.json(1)
         else{
-            res.json(results[0])
+            let result = results1[0]
+            database.query('SELECT `'+type+'` FROM `gracz` WHERE idGracz = ?;',[playerId], function (error, results2) {
+                if (error) res.json(2)
+                else{
+                    let wedki = results2[0]['wedka']
+                    if (wedki!=3) {
+                        wedki = wedki.split(';')
+                        let canBuy = false
+                        wedki.forEach(wedka => {
+                            if (wedka==id) {
+                                canBuy = true
+                            }
+                        });
+                        result['canBuy'] = canBuy
+                        console.log(result);
+                        
+                        res.json(result)
+                    }else res.json(4)
+                }
+            })
         }
     })
 }
