ac826898885e562a3daf0e4b60cc2251de322a9e
kapema <145340839+kapema@users.noreply.github.com>
2024-12-06 15:02:46 +0100

    swap names

--- backend/server/controllers/get/equip.js
+++ backend/server/controllers/get/equip.js
@@ -4,7 +4,7 @@ import querys from './querys.js'
 
 export const equip = (req, res) => {
     const { type, playerId } = req.query
-    database.query(querys.getOwned[type], [playerId], function (error, equips) {
+    database.query(querys.getEquip[type], [playerId], function (error, equips) {
         if (error) res.json(error)
         else {
             let list = (equips[0]["kolowrotek"] + "").slice(0, -1).split(";")
@@ -18,7 +18,7 @@ export const equip = (req, res) => {
                 })
                 query = query.slice(0, -3)
             }
-            query += querys.getEquip[type]
+            query += querys.getOwned[type]
             database.query(query, function (error1, result) {
                 if (error1) res.json(error1)
                 else res.json(result)
--- backend/server/controllers/get/querys.js
+++ backend/server/controllers/get/querys.js
@@ -1,10 +1,10 @@
 const querys = {
-    getOwned: {
+    getEquip: {
         zylka: 'SELECT `zylka` FROM `gracz` WHERE idGracz = ?;',
         kolowrotek: 'SELECT `kolowrotek` FROM `gracz` WHERE idGracz = ?;',
         wedka: 'SELECT `wedka` FROM `gracz` WHERE idGracz = ?;'
     },
-    getEquip: {
+    getOwned: {
         zylka: ',true,false) AS "isOwned" FROM `zylka`',
         kolowrotek: ',true,false) AS "isOwned" FROM `kolowrotek`',
         wedka: ',true,false) AS "isOwned" FROM `wedka'
