5c12ce35466408624b63d904b10d4737cdd842f4
kapema <145340839+kapema@users.noreply.github.com>
2024-12-06 15:10:22 +0100

    update query

--- backend/server/controllers/get/possibilityToBuy.js
+++ backend/server/controllers/get/possibilityToBuy.js
@@ -1,30 +1,12 @@
 import database from '../../database.js'
-
+import querys from './querys.js'
 
 export const possibilityToBuy = (req, res) => {
     const { id, type, playerId } = req.query
-    let cena
-    database.query('SELECT `cena` FROM `' + type + '` WHERE `id` = ?;', [id], function (error1, cenaRes) {
-        if (error1) res.json(null)
+    database.query(querys.possibilityToBuy[type], [id, playerId], function (error, result) {
+        if (error1) res.json(error)
         else {
-            cena = parseInt(cenaRes[0]['cena'])
-            console.log(1);
-
-            database.query('SELECT `pieniadze` FROM `gracz` WHERE `idGracz`=?;', [playerId], function (error2, pieniadzeGracza) {
-                if (error2) res.json(error2)
-                else {
-                    console.log(2);
-
-                    let pieniadze = parseInt(pieniadzeGracza[0]['pieniadze'])
-                    console.log(cena);
-                    console.log(pieniadze);
-
-
-                    if (cena <= pieniadze)
-                        res.json(true)
-                    else res.json(false)
-                }
-            })
+            res.json(result[0]["possib"])
         }
     })
 }
