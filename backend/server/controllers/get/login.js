1ba153ea8c9b8eac4752071352519c3c2315554b
kapema <145340839+kapema@users.noreply.github.com>
2024-12-12 15:41:32 +0100

    login rework

--- backend/server/controllers/get/login.js
+++ backend/server/controllers/get/login.js
@@ -1,24 +1,63 @@
 import database from '../../database.js'
+import bcrypt from 'bcrypt';
 
-
-export const login = (req, res) => {
+export const login = async (req, res) => {
   const { login, password } = req.query;
+  // const salt = await bcrypt.genSalt(); for reg
+  // const hashedPassword = await bcrypt.hash(password, salt);
+  console.log(new Date().toISOString().slice(0, 19).replace('T', ' '))
+  
+  
 
-  database.query('SELECT `id` FROM `dane` WHERE `login` = ? AND `haslo` = ?;', [login, password], function (error, results) {
+  database.query('SELECT `id`,`haslo`,`licznik`,`dataBlokady` FROM `dane` WHERE `login`=?',[login], async function (error, results) {
     if (error) res.json(null)
-    else {
-
-      //   console.log(results);
-
-      let table = JSON.parse(JSON.stringify(results))
-      if (table == null)
-        res.json(null)
-      else {
-        res.json(table[0])
-
+    else{
+      const data = JSON.parse(JSON.stringify(results))[0]
+      console.log(data);
+      if(data){
+        if(data['dataBlokady']!=null){
+          console.log(Date.now());
+          if(data['dataBlokady']+900000>=Date.now())  res.send("jestes zablokowany")
+          else{
+            database.query('UPDATE `dane` SET `licznik`=0,`dataBlokady`=null WHERE `login`=?',[login],  function (error, results) {
+              if (error) res.json(null)
+              else authentication(data)
+            })
+          }
+        }
+        else{
+          authentication(data)
+        }
+      }else{
+        res.send("incorrect login")
       }
-    }
+      }
   })
+  
+  const authentication = async (data) =>{
+    const hashedPassword = data['haslo']
+    const passwordMatched = await bcrypt.compare(password, hashedPassword)
+    if(passwordMatched) {
+      database.query('UPDATE `dane` SET `licznik`=0 WHERE `login`=?',[login],  function (error, results) {
+        if (error) res.json(null)
+        else res.json(data['id'])
+      })
+    }
+    else if(data['licznik']==3){
+      const date = new Date().toISOString().slice(0, 19).replace('T', ' ')
+      database.query('UPDATE `dane` SET `dataBlokady`=? WHERE `login`=?',[date,login],  function (error, results) {
+        if (error) res.json(null)
+        else authentication(data)
+      })
+    }
+    else{
+      database.query('UPDATE `dane` SET `licznik`=`licznik`+1 WHERE `login`=?',[login], async function (error, results) {
+        if (error) res.json(null)
+        else res.send("incorrect password")
+      })
+    }
+  }
+
 }
 
 
