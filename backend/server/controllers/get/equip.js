8c6f6394c8668292ffbde26f97d2bc736294c654
KacperLewa <kacperlewandowski491@gmail.com>
2024-12-13 14:43:08 +0100

    niggers
    xd

---
+++ backend/server/controllers/get/equip.js
@@ -0,0 +1,28 @@
+import database from '../../database.js'
+import querys from './querys.js'
+
+
+export const equip = (req, res) => {
+    const { type, playerId } = req.query
+    database.query(querys.getEquip[type], [playerId], function (error, equips) {
+        if (error) res.json(error)
+        else {
+            let list = (equips[0]["kolowrotek"] + "").slice(0, -1).split(";")
+            let query
+            if (list[0] == '') {
+                query = 'SELECT *, IF(1>2'
+            } else {
+                query = 'SELECT *, IF('
+                list.forEach(id => {
+                    query += '`id`=' + id + ' OR '
+                })
+                query = query.slice(0, -3)
+            }
+            query += querys.getOwned[type]
+            database.query(query, function (error1, result) {
+                if (error1) res.json(error1)
+                else res.json(result)
+            })
+        }
+    })
+}
---
+++ frontend/src/app/shop/shop-service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from "@angular/common/http"
+import { inject, Injectable } from "@angular/core"
+
+import { Equip } from '../app.component';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ShopService{
+    _http = inject(HttpClient);
+    _server = 'http://localhost:3000/';
+
+    getItem(type : string, id : number){
+        return this._http.get<Equip>(this._server+"api/get/equip?id=" + id + "&type=" + type);
+    }
+
+    buyItem(type : string, id : number) : boolean{
+        //tutaj kupowanie
+    }
+}
