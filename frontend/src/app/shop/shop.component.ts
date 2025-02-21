b174771bb95e06a98daa5854738f68fe4f453a01
kapema <kapeciuinstagram@gmail.com>
2025-02-21 13:22:09 +0100

    praca cos

--- frontend/src/app/shop/shop.component.ts
+++ frontend/src/app/shop/shop.component.ts
@@ -29,7 +29,7 @@ export class ShopComponent {
   ]);
   ngOnInit(){
     for(let a : number = 0; a < equipmentTypeArray.length; a++){
-        this.items.set(equipmentTypeArray[a], this._service.getList(equipmentTypeArray[a], 1));
+        this.items.set(equipmentTypeArray[a], this._service.getList('kolowrotek', 1));
         console.log(this.items.get(equipmentTypeArray[a]));
     }
   }
