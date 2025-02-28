343f7ffb90c093fa5febc8c3b5b269fe1000ecea
kapema <kapeciuinstagram@gmail.com>
2025-02-28 12:45:49 +0100

    working on logging in

--- backend/server/routes/player.js
+++ backend/server/routes/player.js
@@ -4,11 +4,12 @@ import { friends } from "../controllers/get/friends.js"
 import { friendsChange } from "../controllers/put/friendsChange.js"
 import { signup } from "../controllers/post/signup.js"
 import { money } from "../controllers/get/money.js"
+import authenticate from "../controllers/get/authenticate.js";
 import { moneyChange } from "../controllers/put/moneyChange.js"
 import testLogin from "../controllers/get/testLogin.js"
 const router = Router()
 
-router.get('/login', login)
+router.get('/login', authenticate, login)
 router.get('/testLogin', testLogin)
 router.post('/signup', signup)  //zmienić na post
 router.get('/znajomi', friends)
